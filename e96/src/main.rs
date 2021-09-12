41a904f454d6a655ad88fb11126056a77a922916
Joseph Dane <joe.dane@gmail.com>
2021-09-11 15:00:17 -1000

    WIP e96

--- e96/src/main.rs
+++ e96/src/main.rs
@@ -2,6 +2,8 @@ use std::fs::File;
 use std::path::Path;
 use std::io::{BufRead, BufReader};
 use std::fmt;
+use std::cmp::Ordering;
+use stringreader::StringReader;
 
 #[derive(Debug, Copy, Clone)]
 struct Item(u32);
@@ -28,10 +30,89 @@ impl PartialEq for Item {
     }
 }
 
+impl PartialOrd for Item {
+
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.0.cmp(&other.0))
+    }
+}
+
 
 type Grid = [[Item; 9]; 9];
 
 type Coord = (usize, usize);
+
+struct ItemIterator {
+    order: u8,
+    step: usize, 
+    next: usize
+}
+
+impl ItemIterator {
+
+    const BY_ROW: u8 = 1;
+    const BY_COL:u8 = 2;
+    const BY_CELL: u8 = 3;
+
+    fn new(order: u8, step: usize) -> Self {
+        ItemIterator { order, step, next: 0 }
+    }
+
+    fn make_coord(&self) -> Coord {
+        if self.order == ItemIterator::BY_ROW {
+            (self.step, self.next)
+        } else if self.order == ItemIterator::BY_COL {
+            (self.next, self.step)
+        } else if self.order == ItemIterator::BY_CELL {
+            let row_start = self.order as usize / 3;
+            let col_start = self.order as usize & 3;
+            (row_start + self.next / 3, col_start + self.next % 3)
+        } else {
+            panic!("invalid order ({}", self.order);
+        }
+    }
+}
+
+impl Iterator for ItemIterator {
+    type Item = Coord;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next < 9 {
+            let n = Some(self.make_coord());
+            self.next += 1;
+            n
+        } else if self.step < 9 {
+            self.next = 0;
+            let n = Some(self.make_coord());
+            self.step += 1;
+            n
+        } else {
+            None
+        }
+    }
+}
+
+struct RowIterator {
+    next: usize
+}
+
+impl RowIterator {
+
+    fn new() -> Self {
+        RowIterator { next:0 }
+    }
+}
+
+impl Iterator for RowIterator {
+    type Item = ItemIterator;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.next < 9 {
+            Some(ItemIterator::new(ItemIterator::BY_ROW, self.next))
+        } else {
+            None
+        }
+    }
+}
 
 #[derive(Clone)]
 struct Board {
@@ -68,26 +149,40 @@ impl Board {
         }
     }
 
-    fn mask_for_value(item: Item) -> u32 {
-        if item <= 0 || item > 9 {
-            panic!("invalid item value");
-        }
-        1 << item
+    fn invert_mask(mask: u32) -> u32 {
+        !mask & 0x1FF
     }
 
-    fn value_for_mask_single(mask: u32) -> Item {
+    fn mask_for_value(item: Item) -> u32 {
+        let val = item.0;
+        if val <= 0 || val > 9 {
+            panic!("invalid item value: {:?}", item);
+        }
+        1 << (val - 1)
+    }
 
+    const MASK_1: u32 = 1u32;
+    const MASK_2: u32 = 1u32 << 1;
+    const MASK_3: u32 = 1u32 << 2;
+    const MASK_4: u32 = 1u32 << 3;
+    const MASK_5: u32 = 1u32 << 4;
+    const MASK_6: u32 = 1u32 << 5;
+    const MASK_7: u32 = 1u32 << 6;
+    const MASK_8: u32 = 1u32 << 7;
+    const MASK_9: u32 = 1u32 << 8;
+    
+    fn value_for_mask_single(mask: u32) -> Item {
         match mask {
-            1 => 1,
-            1 << 1 => Item::from(2),
-            1 << 2 => Item::from(3),
-            1 << 3 => Item::from(4),
-            1 << 4 => Item::from(5),
-            1 << 5 => Item::from(6),
-            1 << 6 => Item::from(7),
-            1 << 7 => Item::from(8),
-            1 << 8 => Item::from(9),
-            _ {
+            i if i == Board::MASK_1 => { Item::from(1) },
+            i if i == Board::MASK_2 => { Item::from(2) },
+            i if i == Board::MASK_3 => { Item::from(3) },
+            i if i == Board::MASK_4 => { Item::from(4) },
+            i if i == Board::MASK_5 => { Item::from(5) },
+            i if i == Board::MASK_6 => { Item::from(6) },
+            i if i == Board::MASK_7 => { Item::from(7) },
+            i if i == Board::MASK_8 => { Item::from(8) },
+            i if i == Board::MASK_9 => { Item::from(9) },
+            _ => {
                 panic!("invalid mask");
             }
         }
@@ -95,13 +190,12 @@ impl Board {
 
     fn value_for_mask_double(mask: u32) -> (Item, Item) {
         let mut i = 1;
-
+        let mut mask = mask;
         if mask == 0 {
             panic!("invalid mask (zero)");
         }
-        
-        while (mask & 1 == 0) {
-            mask >>= mask;
+        while mask & 1 == 0 {
+            mask >>= 1;
             i += 1;
         }
         let item1 = Item::from(i);
@@ -109,11 +203,11 @@ impl Board {
         if mask == 0 {
             panic!("invalid mask (one bit only)");
         }
-        while (mask & 1 == 0) {
+        while mask & 1 == 0 {
             mask >>= 1;
             i+=1;
         }
-        (item1, Item::from(i));
+        (item1, Item::from(i))
     }
     
 
@@ -122,7 +216,7 @@ impl Board {
             let mut tries = vec![];
             let mut mask = 0u32;
             for j in 0..9 {
-                if self.grid[i][j] == 0 {
+                if self.grid[i][j] == Item::from(0) {
                     if tries.len() == 2 {
                         // give up
                         continue 'row;
@@ -130,41 +224,42 @@ impl Board {
                     else {
                         tries.push(j);
                     }
+                } else {
+                    mask |= Self::mask_for_value(self.grid[i][j]);
                 }
-                mask |= Self::mask_for_value(self.grid[i][j]);
             }
             match tries.len() {
 
                 1 => {
-                    self.grid[i][tries[0]] = value_for_mask_single(mask);
-                    Some((i, j))
-                    
+                    self.grid[i][tries[0]] = Board::value_for_mask_single(Board::invert_mask(mask));
+                    return Some((i, tries[0]));
                 },
                 2 => {
-                    let (try1, try2) = value_for_mask_double(mask);
-                    if !self.is_valid((i, tries[0]), try1) {
+                    let (try1, try2) = Board::value_for_mask_double(Board::invert_mask(mask));
+                    if !self.is_valid(&(i, tries[0]), try1) {
                         self.grid[i][tries[1]] = try1;
-                        Some((i, tries[1]))
+                        return Some((i, tries[1]));
                     }
-                    else if !self.is_valid((i, tries[1]), try1) {
+                    else if !self.is_valid(&(i, tries[1]), try1) {
                         self.grid[i][tries[0]] = try1;
-                        Some((i, tries[0]))
+                        return Some((i, tries[0]));
                     }
-                    else if !self.is_valid((i, tries[0]), try2) {
+                    else if !self.is_valid(&(i, tries[0]), try2) {
                         self.grid[i][tries[1]] = try2;
-                        Some((i, tries[1]))
+                        return Some((i, tries[1]));
                     }
-                    else if !self.is_valid((i, tries[1]), try2) {
+                    else if !self.is_valid(&(i, tries[1]), try2) {
                         self.grid[i][tries[0]] = try2;
-                        Some((i. tries[0]))
+                        return Some((i, tries[0]));
                     }
                     else {
-                        None
+                        continue 'row;
                     }
                 },
-                _ => None
+                _ => { continue 'row; }
             }
         }
+        None
     }
     
     
@@ -272,18 +367,21 @@ fn read_board<B: BufRead>(lines: &mut std::io::Lines<B>) -> Result<Board, &'stat
     Ok(Board::new(items))
 }
     
-fn solve(board: &Board) -> Option<Board> {
+fn solve(board: &mut Board) -> Option<Board> {
     if board.empties.len() == 0 {
         Some(board.clone());
     }
     // println!("solving ...");
     // println!("{:?}", *board);
     // println!("empties: {} {:?}", board.empties.len(), board.empties);
+    while let Some(c) = board.try_fill_by_rows() {
+        println!("partial solve: {:?}", c);
+    }
     for empty in &board.empties {
         for test in (1..10).map(|i| Item::from(i)) {
             if board.is_valid(empty, test) {
 //                println!("trying {:?} at index {:?}", test, empty);
-                if let Some(answer) = solve(&board.extend(*empty, test)) {
+                if let Some(answer) = solve(&mut board.extend(*empty, test)) {
                     return Some(answer);
                 }
             }
@@ -293,11 +391,84 @@ fn solve(board: &Board) -> Option<Board> {
 }
 
 fn main() {
-    let boards = read_boards("p096_sudoku.txt").unwrap();
+    let mut boards = read_boards("p096_sudoku.txt").unwrap();
     println!("solving ...");
     println!("{:?}", boards[0]);
-    match solve(&boards[0]) {
+    match solve(&mut boards[0]) {
         None => { println!("No solution") },
         Some(board) => { println!("{:?}", board) }
     }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_read1() {
+        let mut sr = StringReader::new("Test Board\n\
+        123456789\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000");
+        let mut board = read_board(&mut BufReader::new(sr).lines()).unwrap();
+        if let Some(c) = board.try_fill_by_rows() {
+            panic!("test_read_1");
+        }
+    }
+ 
+    #[test]
+    fn test_read2() {
+        let mut sr = StringReader::new("Test Board\n\
+        123456780\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000\n\
+        000000000");
+        let mut board = read_board(&mut BufReader::new(sr).lines()).unwrap();
+        if let Some(c) = board.try_fill_by_rows() {
+            assert_eq!(c, (0, 8));      
+        } else {
+            panic!();
+        }
+    }
+
+    #[test]
+    fn test_read3() {
+        let mut sr = StringReader::new("Test Board\n\
+        123450780\n\
+        000000000\n\
+        000000000\n\
+        000006000\n\
+        000000000\n\
+        000000000\n\
+        000000009\n\
+        000000000\n\
+        000000000");
+        let mut board = read_board(&mut BufReader::new(sr).lines()).unwrap();
+        if let Some(c) = board.try_fill_by_rows() {
+            assert!(c == (0, 5) || c == (0, 8));      
+        } else {
+            panic!();
+        }
+        println!("board is now: {:?}", board);
+
+        if let Some(c) = board.try_fill_by_rows() {
+            assert!(c == (0, 5) || c == (0, 8));      
+        } else {
+            panic!();
+        }
+        println!("{:?}", board);
+    }
+
 }
